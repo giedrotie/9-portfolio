595f467eb4aabfcd4814c705987c61171d073aa0
Giedre <giedrotie@gmail.com>
2021-04-07 00:41:24 +0300

    function for services to automate service card demonstration

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -1,5 +1,32 @@
-function services (data) {
-    console.log(data);
+function services(selector, data) {
+    // input validation
+
+    // logic
+    const DOM = document.querySelector(selector);
+    const { list, imgPath, maxCount } = data;
+
+    let HTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+        const service = list[i];
+
+        if (!service.active) {
+            continue;
+        }
+
+        HTML += `<div class="col-12 col-md-6 col-lg-4 service-item">
+                    <img src="${imgPath + service.img}" alt="">
+                    <h4>${service.title}</h4>
+                    <p>${service.description}</p>
+                    <a href="${service.link}" class="fa fa-long-arrow-right"></a>
+                </div>`;
+    }
+
+
+    // post logic validation
+
+    // result return
+    DOM.innerHTML = HTML;
 }
 
 export { services }
