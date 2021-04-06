3da182b33992f571305631b70fadc5402d7feca9
Giedre <giedrotie@gmail.com>
2021-04-07 00:41:46 +0300

    serviceDate informacijos sarasas objektas

--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -1,26 +1,36 @@
-const servicesDate = [
-    {
-        img: './img/services/mitech-box-image-style-01-image-01-100x108.png',
-        title: 'IT design',
-        description: 'We provide',
-        link: '#',
-        active: true
-    },
-    {
-        img: './img/servicess/mite',
-        title: 'IT design',
-        description: 'We provide',
-        link: '#',
-        active: false
-    },
-    {
-        img: './img/servicess/mite',
-        title: 'IT design',
-        description: 'We provide',
-        link: '#',
-        active: true
-    },
-
-]
+const servicesData = {
+    imgPath: './img/services/',
+    maxCount: 3,
+    list: [
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'IT design',
+            description: 'We provide the most responsive and functional IT design for companies and businesses worldwide.',
+            link: '#',
+            active: true
+        },
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'UX design',
+            description: 'Some demo text',
+            link: '#',
+            active: false
+        },
+        {
+            img: 'mitech-box-image-style-01-image-02-100x108.png',
+            title: 'IT management',
+            description: 'It’s possible to simultaneously manage and transform information from one server to another.',
+            link: '#',
+            active: true
+        },
+        {
+            img: 'mitech-box-image-style-01-image-03-100x108.png',
+            title: 'Data security',
+            description: 'Back up your database, store in a safe and secure place while still maintaining its accessibility.',
+            link: '#',
+            active: true
+        },
+    ]
+}
 
 export { servicesData }
