b294abc1cb6901641e739d8df36b13bb1cc90ff2
Giedre <giedrotie@gmail.com>
2021-04-06 14:36:08 +0300

    servicesData js

---
+++ js/data/servicesData.js
@@ -0,0 +1,26 @@
+const servicesDate = [
+    {
+        img: './img/services/mitech-box-image-style-01-image-01-100x108.png',
+        title: 'IT design',
+        description: 'We provide',
+        link: '#',
+        active: true
+    },
+    {
+        img: './img/servicess/mite',
+        title: 'IT design',
+        description: 'We provide',
+        link: '#',
+        active: false
+    },
+    {
+        img: './img/servicess/mite',
+        title: 'IT design',
+        description: 'We provide',
+        link: '#',
+        active: true
+    },
+
+]
+
+export { servicesData }
