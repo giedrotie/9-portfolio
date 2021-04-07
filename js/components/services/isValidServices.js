76db89280e50ed8e7b96abcfec633e9303776558
Giedre <giedrotie@gmail.com>
2021-04-07 21:58:18 +0300

    service input validation

--- js/components/services/isValidServices.js
+++ js/components/services/isValidServices.js
@@ -1,24 +1,27 @@
-function isValidServices(selector, data) {  
-    if (typeof selector !== 'string' || selector === '') {   //selector turi buti ne tuscias 
+function isValidServices(selector, data) {
+    if (typeof selector !== 'string' ||
+        selector === '') {
+        console.log('ERROR: blogas selectorius');
         return false;
     }
-    if (typeof data !== 'object' || 
-    Array.isArray(data)) || // turi buti tikras objektas, ne array
-    !data.imgPath ||
-    typeof data .imgPath !== 'string' ||
-    data.imgPath === '' ||
-    !data.list ||
-    !Array.isArray(data.list) ||
-    data.list.lenght === 0) {
+
+    if (typeof data !== 'object' ||
+        Array.isArray(data) ||
+        !data.imgPath ||
+        typeof data.imgPath !== 'string' ||
+        data.imgPath === '' ||
+        !data.list ||
+        !Array.isArray(data.list) ||
+        data.list.length === 0) {
+        console.log('ERROR: blogi data duomenys');
         return false;
     }
     
-    if (data.maxCount) {
-        if (typeof data.maxCount !== 'number' ||
-        !isFinite(data.maxCount) ||
-        data.maxCount < 1 ||
-        data.maxCoubt % 1 !== 0) {
-            
+        if (typeof data.maxCount === 'number'){ //jeigu maxcount yra skaicius, 
+            if (!isFinite(data.maxCount) ||         // jis turi buti baigtinis
+            data.maxCount < 1 ||                // ne mazesnis uz 1
+            data.maxCount % 1 !== 0) {          // sveikas skaicius
+            console.log('ERROR: blogas maxCount');
             return false;
         }
     }
