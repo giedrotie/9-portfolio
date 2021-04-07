1c6102b7efc241536215d48239e3108cdb4d07a9
Giedre <giedrotie@gmail.com>
2021-04-07 22:02:35 +0300

    services js

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -1,4 +1,5 @@
-import { isValidServices } from './isValidServices.js'
+import { isValidServices } from './isValidServices.js';
+import { isValidServiceItem } from './isValidServiceItem.js';
 
 function services(selector, data) {
     // input validation
@@ -8,30 +9,36 @@ function services(selector, data) {
 
     // logic
     const DOM = document.querySelector(selector);
-    const { list, imgPath, maxCount } = data; 
+    if (!DOM) {
+        console.error('Pagal pateikta selektoriu nerastas norimas elementas.');
+        return false;
+    }
+
+    const { list, imgPath, maxCount } = data;
 
     let HTML = '';
-    let generatedServicesCount = 0; //kintamasis kuris trackina kelinta paslauga generuoju
-                                    //jeigu maxCounto nera, sugeneruos viska
+    let generatedServicesCount = 0;     //kintamasis kuris trackina kelinta paslauga generuoju
+    //jeigu maxCounto nera, sugeneruos viska
 
     for (let i = 0; i < list.length; i++) {
         const service = list[i];
 
-        if (!service.active) {   //patikra ar paslauga is viso yra aktyvi?
+        if (!isValidServiceItem(service) ||     //patikra ar paslauga is viso yra aktyvi?
+            !service.active) {
             continue;
         }
         if (generatedServicesCount === maxCount) {
             break;
         }
+
         generatedServicesCount++;
-        HTML += `<div class="col-12 col-md-6 col-lg-4 service-item">
+        HTML += `<div class="service-item">
                     <img src="${imgPath + service.img}" alt="">
                     <h4>${service.title}</h4>
                     <p>${service.description}</p>
                     <a href="${service.link}" class="fa fa-long-arrow-right"></a>
                 </div>`;
     }
-
 
     // post logic validation
 
